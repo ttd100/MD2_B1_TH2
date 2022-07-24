b27a3bef50875e32c50f6f9b7f6a841083082487
Your Name <dat054760@gmail.com>
2022-07-24 22:04:15 +0700

    up

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+        public static void main(String[] args) {
+            int i = 10;
+            float f = 20.5f;
+            boolean b = true;
+            char c = 'a';
+            String s = "Ha Noi";
+
+            System.out.println("i = " + i);
+            System.out.println("f = " + f);
+            System.out.println("b = " + b);
+            System.out.println("c = " + c);
+            System.out.printf("s = " + s);
+        }
+}
